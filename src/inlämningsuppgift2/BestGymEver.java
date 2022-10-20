fc2f5b06e00c2d4862a1b183b480ee6b2de5fdb7
Maria Hagelin <mthagelin@gmail.com>
2022-10-20 19:44:01 +0200

    trim()

--- src/inlämningsuppgift2/BestGymEver.java
+++ src/inlämningsuppgift2/BestGymEver.java
@@ -19,12 +19,12 @@ public class BestGymEver {
                         "3=Sök efter något som står i kundens personliga fil\n4=Kolla totala antal träningstillfällen för kund\n5=Avsluta");
                 if (val.trim().equals("1")) {
                     seStatus = true;
-                    String personnummer = JOptionPane.showInputDialog("Vilken kund? Ange personnummer (10 siffror)");
+                    String personnummer = JOptionPane.showInputDialog("Vilken kund? Ange personnummer (10 siffror)").trim();
                     if (member.korrektPersonnummer(personnummer, test) == true) {
                         member.finnsIFil(personnummer, filväg, test, seStatus);
                     }
                 } else if (val.trim().equals("2")) {
-                    String personnummer = JOptionPane.showInputDialog("Vilken kund? Ange personnummer (10 siffror)");
+                    String personnummer = JOptionPane.showInputDialog("Vilken kund? Ange personnummer (10 siffror)").trim();
                     if (member.korrektPersonnummer(personnummer, test) == true && member.finnsIFil(personnummer, filväg, test, seStatus) == 2) {
                         String kundensFilväg = "src/inlämningsuppgift2/" + personnummer + ".txt";
                         member.kundHarTränatTillPTFil(personnummer, filväg, test);
@@ -32,7 +32,7 @@ public class BestGymEver {
                         JOptionPane.showMessageDialog(null, "Det går inte att söka för icke-medlem");
                     }
                 } else if (val.trim().equals("3")) {//observera att kunden måste ha loggat träning för att det ska finnas en fil att söka i
-                    String personnummer = JOptionPane.showInputDialog("Vilken kund vill du söka för? Ange personnummer (10 siffror)");
+                    String personnummer = JOptionPane.showInputDialog("Vilken kund vill du söka för? Ange personnummer (10 siffror)").trim();
                     if (member.korrektPersonnummer(personnummer, test) == true && member.finnsIFil(personnummer, filväg, test, seStatus) == 2) {
                         String sökord = JOptionPane.showInputDialog("Ange vad du letar efter i filen\n(t.ex. ett träningstillfälle 2019-07-08)");
                         Path kundensPath = Paths.get((member.returneraPathnamn(personnummer)));
@@ -41,7 +41,7 @@ public class BestGymEver {
                         JOptionPane.showMessageDialog(null, "Det går inte att söka för icke-medlem");
                     }
                 } else if (val.trim().equals("4")) {
-                    String personnummer = JOptionPane.showInputDialog("Vilken kund vill du söka för? Ange personnummer (10 siffror)");
+                    String personnummer = JOptionPane.showInputDialog("Vilken kund vill du söka för? Ange personnummer (10 siffror)").trim();
                     if (member.korrektPersonnummer(personnummer, test) == true && member.finnsIFil(personnummer, filväg, test, seStatus) == 2) {
                         Path kundensPath = Paths.get(member.returneraPathnamn(personnummer));
                         JOptionPane.showMessageDialog(null, "Antal träningstillfällen: " +
@@ -50,6 +50,7 @@ public class BestGymEver {
                         JOptionPane.showMessageDialog(null, "Det går inte att söka för icke-medlem");
                     }
                 } else if (val.trim().equals("5")) {
+
                     kör = false;
                 } else {
                     JOptionPane.showMessageDialog(null, "Du angav varken 1, 2, 3, 4 eller 5");
